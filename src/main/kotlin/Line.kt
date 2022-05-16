02ea74fbd450bba54d30ae593ea54c871f948633
Nate Thomson <n8thomson@outlook.com>
2022-05-15 23:58:40 -0600

    line and slope

--- src/main/kotlin/Line.kt
+++ src/main/kotlin/Line.kt
@@ -1,2 +1,27 @@
-class Line {
+class Line(p1: Point = Point (0.0, 0.0), p2: Point = Point (1.0, 0.0)) {
+    var p1: Point = Point(0.0, 0.0)
+        set (value){
+            if (value.isEqual(p2)){
+                field = p1
+            } else {
+                field = value
+            }
+        }
+    var p2: Point = Point(1.0, 0.0)
+        set (value){
+            if (value.isEqual(p1)){
+                field = p2
+            } else {
+                field = value
+            }
+        }
+
+    init {
+        this.p1 = p1
+        this.p2 = p2
+    }
+
+    fun getSlope(): Double{
+        return ((p2.y - p1.y) / (p2.x/p1.x))
+    }
 }
--- src/main/kotlin/Point.kt
+++ src/main/kotlin/Point.kt
@@ -1,2 +1,39 @@
-class Point {
+class Point(x: Double = 0.0, y: Double = 0.0) {
+    var x: Double = 0.0
+//        set (value){
+//            if (value < 0.0){
+//                field = 0.0
+//            } else {
+//                field = value
+//            }
+//        }
+    var y: Double = 0.0
+//        set (value){
+//            if (value < 0.0){
+//                field = 0.0
+//            } else {
+//                field = value
+//            }
+//        }
+    init {
+        this.x = x
+        this.y = y
+    }
+
+    fun clone(): Point{
+        return Point(this.x, this.y)
+    }
+
+    fun move(deltaX: Double, deltaY: Double){
+        this.x += deltaX
+        this.y += deltaY
+    }
+
+    fun isEqual(p: Point): Boolean{
+        if (this.x == p.x && this.y == p.y){
+            return true
+        }
+        return false
+    }
+
 }
