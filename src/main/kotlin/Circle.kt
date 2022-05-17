ce4a8fe97af91e7d42db3ea087bb9cbc73d6370a
Nate Thomson <n8thomson@outlook.com>
2022-05-16 20:40:20 -0600

    Circle and Ellipse

--- src/main/kotlin/Circle.kt
+++ src/main/kotlin/Circle.kt
@@ -1,3 +1,20 @@
-class Circle(p: Point, r1: Double, r2: Double): Ellipse(p, r1, r2) {
+class Circle(p: Point = Point(0.0, 0.0), r1: Double = 1.0, r2: Double = 1.0): Ellipse(p, r1, r2) {
 
+    override var r1: Double = 1.0
+        set (value){
+            if (!Circle(p, value, r2).isCircle()){
+                field = r1
+            } else {
+                field = value
+            }
+        }
+
+    override var r2: Double = 1.0
+        set (value){
+            if (!Circle(p, r1, value).isCircle()){
+                field = r2
+            } else {
+                field = value
+            }
+        }
 }
--- src/main/kotlin/Triangle.kt
+++ src/main/kotlin/Triangle.kt
@@ -1,2 +1,33 @@
-class Triangle {
+class Triangle(p1: Point = Point(0.0, 0.0), p2: Point = Point(1.0, 0.0), p3: Point = Point(0.0, 1.1)) {
+    var p1: Point = Point(0.0, 0.0)
+        set (value){
+            if (value.isEqual(p2)){
+                field = p1
+            } else {
+                field = value
+            }
+        }
+    var p2: Point = Point(1.0, 0.0)
+        set (value) {
+            if (value.isEqual(p1)) {
+                field = p2
+            } else {
+                field = value
+            }
+        }
+    var p3: Point = Point(0.0, 0.0)
+        set (value){
+            if (value.isEqual(p2)){
+                field = p1
+            } else {
+                field = value
+            }
+        }
+
+
+    fun getArea(): Double{
+        return (p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y))
+    }
+
+
 }
